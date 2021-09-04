effcccb8325c0ebfbced1bcee5045b8182244953
Vijayakrishna M Ramachandra <32487859+vijayakrishnamr@users.noreply.github.com>
2021-09-04 12:14:58 +0530

    Additional details to console

--- GRPC/VehicleTrackerConsumer/Program.cs
+++ GRPC/VehicleTrackerConsumer/Program.cs
@@ -16,30 +16,35 @@ namespace VehicleTrackerConsumer
             using (var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001/"))
             {
                 var client = new VehicleTracker.TrackerService.TrackerServiceClient(channel);
+                var sw2 = new Stopwatch();
                 for (int i = 0; i < 100; i++)
                 {
-
-
+                    sw2.Restart();
                     var vehicleReg = Guid.NewGuid();
                     Console.WriteLine($"Create new Vehicle {vehicleReg}");
                     var vehicleMessage = new VehicleMessage() { Registration = vehicleReg.ToString(), VehicleType = "Car" };
                     var response = client.CreateVehicle(vehicleMessage);
-                    vehicleMessage.Id = response.Vehicle.Id;
-                    Console.WriteLine($"Created Vehicle with Id {response.Vehicle.Id} - {DateTime.Now}");
+                    sw2.Stop();
+                    Console.WriteLine($"Created Vehicle with Id {response.Vehicle.Id} - {DateTime.Now}, {sw2.ElapsedMilliseconds}");
 
                     for (int j = 0; j < 10; j++)
                     {
-                        var vehicleMovement = new VehicleMovementMessage() { Vehicle = vehicleMessage, ActionTime = DateTime.UtcNow.ToString(), ActionType = "Test" };
+                        sw2.Restart();
+                        var vehicleMovement = new VehicleMovementMessage() { Vehicle = response.Vehicle, ActionTime = DateTime.UtcNow.ToString(), ActionType = "Test" };
                         client.AddMovement(vehicleMovement);
-                        Console.WriteLine($"Created Vehicle Movement. {DateTime.Now}");
+                        sw2.Stop();
+                        Console.WriteLine($"Created Vehicle Movement. {DateTime.Now}, {sw2.ElapsedMilliseconds}");
                     }
                 }
 
                 var addedVehicles = client.GetVehicles(new QueryRequest());
+                Console.WriteLine($"Total {addedVehicles.Vehicles.Count} vehicles fetched");
                 foreach (var addedVehicle in addedVehicles.Vehicles)
                 {
+                    sw2.Restart();
                     client.DeleteVehicle(addedVehicle);
-                    Console.WriteLine($"Deleted Vehicle {addedVehicle.Id} - {DateTime.Now}");
+                    sw2.Stop();
+                    Console.WriteLine($"Deleted Vehicle {addedVehicle.Id} - {DateTime.Now}, {sw2.ElapsedMilliseconds}");
                 }
             }
 
--- REST/VehicleTrackerConsumer/Program.cs
+++ REST/VehicleTrackerConsumer/Program.cs
@@ -17,8 +17,10 @@ namespace VehicleTrackerConsumer
             Stopwatch sw = new Stopwatch();
             sw.Start();
 
+            Stopwatch sw2 = new Stopwatch();
             for (int i = 0; i < 100; i++)
             {
+                sw2.Restart();
                 var vehicleReg = Guid.NewGuid();
                 Console.WriteLine($"Create new Vehicle {vehicleReg}");
                 var vehicleMessage = new VehicleDto() { Registration = vehicleReg.ToString(), VehicleType = "Car" };
@@ -26,22 +28,25 @@ namespace VehicleTrackerConsumer
                 using (var client = new WebClient())
                 {
                     client.Headers[HttpRequestHeader.ContentType] = "application/json";
-                    var response = client.UploadString("https://localhost:44321/api/vehicles", "POST",
+                    var response = client.UploadString("https://localhost:5001/api/vehicles", "POST",
                         JsonConvert.SerializeObject(vehicleMessage));
                     var createdVehicle = JsonConvert.DeserializeObject<VehicleDto>(response);
                     vehicleMessage.Id = createdVehicle.Id;
-                    Console.WriteLine($"Created Vehicle {createdVehicle.Id} - {DateTime.Now}");
+                    sw2.Stop();
+                    Console.WriteLine($"Created Vehicle {createdVehicle.Id} - {DateTime.Now}, {sw2.ElapsedMilliseconds}");
                 }
 
                 for (int j = 0; j < 10; j++)
                 {
+                    sw2.Restart();
                     var vehicleMovement = new VehicleMovementDto() { Vehicle = vehicleMessage, ActionTime = DateTime.UtcNow.ToString(), ActionType = "Test" };
                     using (var client = new WebClient())
                     {
                         client.Headers[HttpRequestHeader.ContentType] = "application/json";
-                        var response = client.UploadString("https://localhost:44321/api/vehiclemovements", "POST",
+                        var response = client.UploadString("https://localhost:5001/api/vehiclemovements", "POST",
                             JsonConvert.SerializeObject(vehicleMovement));
-                        Console.WriteLine($"Created Vehicle Movement. {DateTime.Now}");
+                        sw2.Stop();
+                        Console.WriteLine($"Created Vehicle Movement. {DateTime.Now}, {sw2.ElapsedMilliseconds}");
                     }
                 }
             }
@@ -50,17 +55,20 @@ namespace VehicleTrackerConsumer
             using (var client = new WebClient())
             {
                 client.Headers[HttpRequestHeader.ContentType] = "application/json";
-                var response = client.DownloadString("https://localhost:44321/api/vehicles");
+                var response = client.DownloadString("https://localhost:5001/api/vehicles");
                 vehicles = JsonConvert.DeserializeObject<IList<VehicleDto>>(response);
+                Console.WriteLine($"Total {vehicles.Count} vehicles fetched");
             }
 
             foreach (var vehicleDto in vehicles)
             {
+                sw2.Restart();
                 using (var client = new WebClient())
                 {
                     client.Headers[HttpRequestHeader.ContentType] = "application/json";
-                    var response = client.UploadString("https://localhost:44321/api/vehicles", "DELETE", JsonConvert.SerializeObject(vehicleDto));
-                    Console.WriteLine($"Deleted Vehicle {vehicleDto.Id} -  {DateTime.Now}");
+                    var response = client.UploadString("https://localhost:5001/api/vehicles", "DELETE", JsonConvert.SerializeObject(vehicleDto));
+                    sw2.Stop();
+                    Console.WriteLine($"Deleted Vehicle {vehicleDto.Id} -  {DateTime.Now}, {sw2.ElapsedMilliseconds}");
                 }
             }
 
